97ebe430223a1f558d8935f189166334b3636f60
Devibala-R <105733822+Devibala-R@users.noreply.github.com>
2023-06-01 00:45:41 +0530

    feature commit 1
    Added a game manager.

---
+++ Floor_is_lava/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            return instance;
+        } 
+    }
+
+   
+    private void Awake()
+    {
+        instance = this;
+    }
+
+
+}
